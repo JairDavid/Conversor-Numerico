55feecc55436ac70a27be3d32617edd2b7589173
Jair David <67834146+JairDavid@users.noreply.github.com>
2020-12-20 00:32:53 -0600

    Add files via upload

---
+++ ConversorNumerico/ConversorNumerico/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace ConversorNumerico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConversion = new System.Windows.Forms.Button();
+            this.btnMemoria = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(131)))), ((int)(((byte)(143)))));
+            this.panel1.Controls.Add(this.lb2);
+            this.panel1.Controls.Add(this.lb1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btnConversion);
+            this.panel1.Controls.Add(this.btnMemoria);
+            this.panel1.Location = new System.Drawing.Point(265, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(473, 251);
+            this.panel1.TabIndex = 3;
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.Location = new System.Drawing.Point(12, 103);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(135, 21);
+            this.lb2.TabIndex = 5;
+            this.lb2.Text = "Número Binario: ";
+            this.lb2.Visible = false;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(12, 38);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(148, 21);
+            this.lb1.TabIndex = 4;
+            this.lb1.Text = "Número Decimal: ";
+            this.lb1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(194, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(265, 27);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(194, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
+            // 
+            // btnConversion
+            // 
+            this.btnConversion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConversion.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConversion.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnConversion.Location = new System.Drawing.Point(238, 176);
+            this.btnConversion.Name = "btnConversion";
+            this.btnConversion.Size = new System.Drawing.Size(183, 45);
+            this.btnConversion.TabIndex = 1;
+            this.btnConversion.Text = "Realizar Conversion";
+            this.btnConversion.UseVisualStyleBackColor = true;
+            this.btnConversion.Visible = false;
+            this.btnConversion.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btnMemoria
+            // 
+            this.btnMemoria.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMemoria.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemoria.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMemoria.Location = new System.Drawing.Point(39, 176);
+            this.btnMemoria.Name = "btnMemoria";
+            this.btnMemoria.Size = new System.Drawing.Size(183, 45);
+            this.btnMemoria.TabIndex = 0;
+            this.btnMemoria.Text = "Vaciar Memoria";
+            this.btnMemoria.UseVisualStyleBackColor = true;
+            this.btnMemoria.Visible = false;
+            this.btnMemoria.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(131)))), ((int)(((byte)(143)))));
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Controls.Add(this.button6);
+            this.flowLayoutPanel1.Controls.Add(this.button7);
+            this.flowLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 46);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(247, 251);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Decimal A Binario";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(3, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Binario A Decimal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(3, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(220, 56);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Decimal A Octal";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(3, 189);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(220, 56);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Octal A Decimal ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Location = new System.Drawing.Point(3, 251);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(220, 56);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Decimal A Hexadecimal";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Location = new System.Drawing.Point(3, 313);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(220, 56);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Hexadecimal A Decimal";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(39, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Conversor Numerico";
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Black;
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.ForeColor = System.Drawing.Color.Transparent;
+            this.button9.Location = new System.Drawing.Point(700, 5);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(37, 36);
+            this.button9.TabIndex = 2;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(747, 309);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnMemoria;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btnConversion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ConversorNumerico/ConversorNumerico/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversorNumerico{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e){
+            if (btnMemoria.Visible){
+                lb1.Text = "Número Binario: ";
+                lb2.Text = "Número Decimal: ";
+            }
+            else{
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                lb1.Visible = true;
+                lb2.Visible = true;
+                btnConversion.Visible = true;
+                btnMemoria.Visible = true;
+                lb1.Text = "Número Binario: ";
+                lb2.Text = "Número Decimal: ";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e){
+            if (btnMemoria.Visible){
+                lb1.Text = "Número Decimal: ";
+                lb2.Text = "Número Binario: ";
+            }
+            else{
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                lb1.Visible = true;
+                lb2.Visible = true;
+                btnConversion.Visible = true;
+                btnMemoria.Visible = true;
+                lb1.Text = "Número Binario: ";
+                lb1.Text = "Número Decimal: ";
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e){
+            if (btnMemoria.Visible){
+                lb1.Text = "Número Decimal: ";
+                lb2.Text = "Número Octal: ";
+            }
+            else{
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                lb1.Visible = true;
+                lb2.Visible = true;
+                btnConversion.Visible = true;
+                btnMemoria.Visible = true;
+                lb1.Text = "Número Decimal: ";
+                lb2.Text = "Número Octal: ";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e){
+            if (btnMemoria.Visible){
+                lb1.Text = "Número Octal: ";
+                lb2.Text = "Número Decimal: ";
+            }
+            else{
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                lb1.Visible = true;
+                lb2.Visible = true;
+                btnConversion.Visible = true;
+                btnMemoria.Visible = true;
+                lb1.Text = "Número Octal: ";
+                lb2.Text = "Número Decimal: ";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e){
+            if (btnMemoria.Visible){
+                lb1.Text = "Número Decimal: ";
+                lb2.Text = "Número Hexadecimal: ";
+            }
+            else{
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                lb1.Visible = true;
+                lb2.Visible = true;
+                btnConversion.Visible = true;
+                btnMemoria.Visible = true;
+                lb1.Text = "Número Decimal: ";
+                lb2.Text = "Número Hexadecimal: ";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e){
+            if (btnMemoria.Visible){
+                lb1.Text = "Número Hexadecimal: ";
+                lb2.Text = "Número Decimal: ";
+            }
+            else{
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                lb1.Visible = true;
+                lb2.Visible = true;
+                btnConversion.Visible = true;
+                btnMemoria.Visible = true;
+                lb1.Text = "Número Hexadecimal: ";
+                lb2.Text = "Número Decimal: ";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e){
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button8_Click(object sender, EventArgs e){
+            if(lb1.Text== "Número Decimal: "&& lb2.Text== "Número Binario: "){
+                decimalBinario();
+
+            }else if(lb1.Text == "Número Binario: " && lb2.Text == "Número Decimal: "){
+                binarioDecimal();
+
+            }else if(lb1.Text == "Número Decimal: " && lb2.Text == "Número Octal: "){
+                decimalOctal();
+
+            }else if(lb1.Text == "Número Octal: " && lb2.Text == "Número Decimal: "){
+                octalDecimal();
+
+            }else if(lb1.Text == "Número Decimal: " && lb2.Text == "Número Hexadecimal: "){
+                decimalHexadecimal();
+
+            }else if(lb1.Text == "Número Hexadecimal: " && lb2.Text == "Número Decimal: "){
+                hexadecimalDecimal();
+            }
+        }
+        private void decimalBinario(){
+            try{
+                long numero, residuo;
+                String ordenar = "";
+                numero = Convert.ToInt64(textBox1.Text);
+                while (numero != 0){
+                    residuo = numero % 2;
+                    numero = numero / 2;
+                    textBox2.Text += residuo;
+                }
+                String binario = textBox2.Text;
+                textBox2.Clear();
+                for (int i = binario.Length - 1; i >= 0; i--){
+                    ordenar += binario[i];
+                }
+                textBox2.Text = ordenar;
+            }catch{
+                MessageBox.Show("Error","Entrada Invalida.",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }  
+        }
+        private void binarioDecimal(){
+            try{
+                string numerobinario;
+                string digitos = "01";
+                char caracter;
+                numerobinario = textBox1.Text;
+                int d, valorDecimal = 0;
+                for (int i = 0; i <= numerobinario.Length - 1; i++){
+                    caracter = numerobinario[i];
+                    d = digitos.IndexOf(caracter);
+                    valorDecimal = 2 * valorDecimal + d;
+                }
+                textBox2.Text = Convert.ToString(valorDecimal);
+            }catch{
+                MessageBox.Show("Error", "Entrada Invalida.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+        }
+        private void decimalOctal(){
+            try{
+                long Decimal, residuo;
+                string ordenar = "";
+                Decimal = Convert.ToInt64(textBox1.Text);
+                while (Decimal > 0){
+                    residuo = Decimal % 8;
+                    Decimal = Decimal / 8;
+                    textBox2.Text += residuo;
+                }
+                string numeroDecimal = textBox2.Text;
+                textBox2.Clear();
+                for (int i = numeroDecimal.Length - 1; i >= 0; i--){
+                    ordenar += numeroDecimal[i];
+                }
+                textBox2.Text = Convert.ToString(ordenar);
+            }catch{
+                MessageBox.Show("Error", "Entrada Invalida.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        }
+        private void octalDecimal(){
+            try{
+                string numeroOctal;
+                string digitos = "01234567";
+                char caracter;
+                numeroOctal = textBox1.Text;
+                int d, valorDecimal = 0;
+                for (int i = 0; i <= numeroOctal.Length - 1; i++){
+                    caracter = numeroOctal[i];
+                    d = digitos.IndexOf(caracter);
+                    valorDecimal = 8 * valorDecimal + d;
+                }
+                textBox2.Text = Convert.ToString(valorDecimal);
+            }catch{
+                MessageBox.Show("Error", "Entrada Invalida.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+        private void decimalHexadecimal(){
+            try{
+                long nDecimal, rem;
+                String hexadecimal = "", valorHex;
+                char[] caracteres = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+                nDecimal = Convert.ToInt64(textBox1.Text);
+                while (nDecimal > 0){
+                    rem = nDecimal % 16;
+                    hexadecimal = caracteres[rem] + hexadecimal;
+                    nDecimal = nDecimal / 16;
+                }
+                textBox2.Text = Convert.ToString(hexadecimal);
+            }catch{
+                MessageBox.Show("Error", "Entrada Invalida.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+        private void hexadecimalDecimal(){
+            try{
+                string numeroHexadecimal;
+                string digitos = "0123456789ABCDEF";
+                char caracter;
+                numeroHexadecimal = textBox1.Text;
+                numeroHexadecimal = numeroHexadecimal.ToUpper();
+                int d, valorDecimal = 0;
+                for (int i = 0; i <= numeroHexadecimal.Length - 1; i++){
+                    caracter = numeroHexadecimal[i];
+                    d = digitos.IndexOf(caracter);
+                    valorDecimal = 16 * valorDecimal + d;
+                }
+                textBox2.Text = Convert.ToString(valorDecimal);
+            }
+            catch{
+                MessageBox.Show("Error", "Entrada Invalida.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
